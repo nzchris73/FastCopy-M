8e6513324c8155d7569ab2b8413b0d71ee24fca7
nzchris73 <103698808+nzchris73@users.noreply.github.com>
2023-08-30 14:31:44 +1200

    Updated xxhash.h. Bumped version to 3.7.0.32

--- src/version.cpp
+++ src/version.cpp
@@ -1,5 +1,5 @@
 ﻿static char *version_id =
-	"@(#)Copyright (C) 2004-2019 H.Shirouzu and FastCopy Lab, LLC.	Version.cpp ver3.7.0.31b"
+	"@(#)Copyright (C) 2004-2019 H.Shirouzu and FastCopy Lab, LLC.	Version.cpp ver3.7.0.32"
 #ifdef _DEBUG
 	"d"
 #endif
