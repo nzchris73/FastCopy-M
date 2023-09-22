3ab6f7dde4b37676caae36c75c31a720f4a9f2bb
nzchris73 <103698808+nzchris73@users.noreply.github.com>
2023-09-22 16:40:19 +1200

    Updated zlib dev files. Bumped version to 3.7.0.33

--- src/version.cpp
+++ src/version.cpp
@@ -1,5 +1,5 @@
 ﻿static char *version_id =
-	"@(#)Copyright (C) 2004-2019 H.Shirouzu and FastCopy Lab, LLC.	Version.cpp ver3.7.0.32"
+	"@(#)Copyright (C) 2004-2019 H.Shirouzu and FastCopy Lab, LLC.	Version.cpp ver3.7.0.33"
 #ifdef _DEBUG
 	"d"
 #endif
